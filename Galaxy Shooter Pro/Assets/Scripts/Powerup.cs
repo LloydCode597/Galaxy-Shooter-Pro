f353088d874f1dfb15cb07393c20bcc9cbf3b7ec
Lloyd Allen <lloydallen597@gmail.com>
2023-07-15 02:08:20 -0500

    Triple Shot Powerup Behaviour

---
+++ Galaxy Shooter Pro/Assets/Scripts/Powerup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -4.5f)
+        {
+            Destroy(this.gameObject);
+        }    
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
